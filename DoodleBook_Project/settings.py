6c7660f50095e699ed029083eb6ac27b00d146c1
Derek-Mactal <dejumactal@gmail.com>
2021-03-04 13:58:26 -0800

    changed the settings.py secretkey config

--- DoodleBook_Project/settings.py
+++ DoodleBook_Project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 from decouple import config
 import os
 
-SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
